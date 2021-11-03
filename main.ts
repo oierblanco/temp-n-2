4f80d4e702c6e77691f6b55ef04c36b7ba625d50
oierblanco <93262346+oierblanco@users.noreply.github.com>
2021-11-03 14:27:49 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let temperatura = 0
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (true) {
+        temperatura = input.temperature()
+        basic.showNumber(temperatura - 0)
+    } else if (input.temperature() < 16) {
+    	
+    } else {
+    	
+    }
+})
